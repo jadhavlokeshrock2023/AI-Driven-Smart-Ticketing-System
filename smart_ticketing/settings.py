d67f26e7b85fbe9d9dad1261fea133843f90b930
lokesh <jadhavlokeshrock2023@gmail.com>
2026-07-27 09:26:34 +0530

    Configure PostgreSQL database

--- smart_ticketing/settings.py
+++ smart_ticketing/settings.py
@@ -121,7 +121,7 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
     )
 }
