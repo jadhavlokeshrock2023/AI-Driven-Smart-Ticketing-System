883e65cfdb6ef4fa96286ae546bff070c0030bb6
lokesh <jadhavlokeshrock2023@gmail.com>
2026-07-27 07:51:41 +0530

    configure groq ai

--- ai/groq.py
+++ ai/groq.py
@@ -6,9 +6,17 @@ import os
 load_dotenv()
 
 
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY is missing")
+
+
 client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key=GROQ_API_KEY
 )
+
 
 
 def ask_groq(message):
